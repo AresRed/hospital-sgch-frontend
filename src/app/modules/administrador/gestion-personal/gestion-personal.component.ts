42d4ceba7187b484febd3bfb0c2498e108bef712
AresRed <ares19951208@gmail.com>
2025-10-22 15:36:13 -0500

    arquitectura de software

---
+++ src/app/modules/administrador/gestion-personal/gestion-personal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-personal',
+  imports: [],
+  templateUrl: './gestion-personal.component.html',
+  styleUrl: './gestion-personal.component.scss'
+})
+export class GestionPersonalComponent {
+
+}
